c8a5013523417eb5c3d9ec4b55f45796a8d42958
ismet <kadyrovismet1999@gmail.com>
2025-05-12 21:10:08 +0500

    feat: added project configs

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { createServer } from 'http';
+
+const PORT = 3000;
+
+const server = createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
